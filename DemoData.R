e762b58d95694e6a5fc2e54963da1b5060d86f2a
pallavi101325 <72800169+pallavi101325@users.noreply.github.com>
2023-01-10 10:46:22 +0530

    Add files via upload

---
+++ DemoData.R
@@ -0,0 +1,14 @@
+SFD_mdm <- hashmap()
+SFD_mdm[["AP"]] <- c(3, 14, 15, 16, 31)
+SFD_mdm[["GU"]] <- c(1, 2, 3, 14, 15, 16, 31)
+SFD_mdm[["OD"]] <- c(1, 2, 14, 15, 31)
+SFD_mdm[["ANI"]]<- c(16, 31)
+
+
+SFD_om = hashmap()
+
+SFD_om[["AP"]]<- c(3, 4, 5, 14,15, 16, 31)
+SFD_om[["MA"]] <- c(3)
+SFD_om[["KE"]] <- c(2, 14, 15, 16, 31)
+SFD_om[["WB"]] <- c(4, 5, 14, 15)
+SFD_om[["ANI"]]  <- c(2)
